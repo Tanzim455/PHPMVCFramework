4b4404d78af557fe05747c7ea7c33f9dbd976c29
Tanzim455 <tanzim.ibthesam@gmail.com>
2024-11-25 12:41:21 +0600

    Class Controller and method check done

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
-<?php 
+<?php
+
+// Retrieve controller and action from the query parameters
+$controller = $_GET['controller'];
+$action = $_GET['action'];
+//Dumping the controller
+
+var_dump($controller);
+$controllerFile = "./src/Controller/" . $controller . ".php";
 
 
+// Check if the controller file exists
+if (file_exists($controllerFile)) {
+    include $controllerFile;
 
-$controller=$_GET['controller'];
+    // Check if the class exists
+    if (class_exists(class:$controller)) {
+        $about = new $controller();
 
-$action=$_GET['action'];
-
-
-
-    include "./src/Controller/$controller.php";
-    $about=new $controller();
-    $about->$action();
-
-?>
-
-
+        // Check if the action method exists
+        if (method_exists($about, $action)) {
+            // Call the action method
+            $about->$action();
+        } else {
+            echo "The method `$action` does not exist in the `$controller` class.";
+        }
+    } else {
+        echo "Class `$controller` does not exist.";
+    }
+} else {
+    echo "Controller file for `$controller` does not exist.";
+}
