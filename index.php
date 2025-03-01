0c4bd5344306b41f80dbcd630f1aa3cbc80ea5b6
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-03-02 00:11:46 +0600

    Ht access file entry point fixed to public/index.php

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
 
 
-echo "Hello World";
-
-$method=$_SERVER['REQUEST_METHOD'];
-
-$parse_url=parse_url($_SERVER['REQUEST_URI']);
-
-var_dump($parse_url["path"]);
+var_dump("Entry point of application");
 
