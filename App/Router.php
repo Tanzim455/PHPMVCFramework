3e66028f27ec2ca6033d2c643a2b4d2aa0db29f5
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-03-02 16:41:36 +0600

    Routes not corrected properly

--- App/Router.php
+++ App/Router.php
@@ -2,34 +2,56 @@
 declare (strict_types=1);
 namespace App;
 class Router{
+    private $routes_list=[];
     private $routes=[];
    
 
     
 
         public function get(string $method,string $path):void {
-            $this->routes[]=$path;
           
+            
+             $this->routes_list[]=$path;
+             
+            foreach($this->routes_list as $route){
+                        
+                
+                array_push($this->routes,$route);
+            }
+                echo "<pre>";
+                
+                echo "</pre>";
             // Get the REQUEST_URI
+            //Merge all arrays 
+            $allArrays_routes = array_merge(...$this->routes);
+            var_dump($allArrays_routes);
+
+            
+            
             $requestUri = $_SERVER['REQUEST_URI'];
             
             $parsedUrl = parse_url($requestUri);
-            var_dump("The Parsed url is");
+            
             
             //Get the exact Url 
             
-           print_r($parsedUrl);
+         
            //Find url without params
 
         //    $url=explode(separator:'/',string:trim($parsedUrl['path']));
 
         //    var_dump($url);
-            $trim=$parsedUrl;
+            $path_of_user=$parsedUrl['path'];
 
-            var_dump(trim($trim['path']));
-            //Filter out all arrays which are not NUll
-        
-        
+            
+          
+            //Convert string to an array
+            $explode=explode(separator:"/",string:$path_of_user);
+
+            //    var_dump($explode);
+
+              //All paths of methods
+
 
     }
 }
--- public/index.php
+++ public/index.php
@@ -7,17 +7,10 @@ $router=new Router();
 require_once '../routes.php';
 
 
-$method=$_SERVER['REQUEST_METHOD'];
-
-var_dump($method);
-
-$parse_url=parse_url($_SERVER['REQUEST_URI']);
-
-var_dump($parse_url);
 
 
 
-// $router->get(method:$_SERVER['REQUEST_METHOD'],path:'two');
+
 
 
 
