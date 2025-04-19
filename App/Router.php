27400bfbb581d7110ad389ce5842002d40043892
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-04-20 05:43:37 +0600

    Route exists

--- App/Router.php
+++ App/Router.php
@@ -5,13 +5,13 @@ namespace App;
 
 class Router {
     // Store routes (with path and method)
-    public array $methods=['GET','POST'];
-    public function get(?string $path = null, ?string $method = null):array {
+    public array $allowed_methods=['GET','POST'];
+    public function get(?string $path = null):array {
         static $routes = [];
         
         // If path is provided, store the route with the method
-        if ($path !== null && $method !== null) {
-            $routes[] = ['path' => $path, 'method' => $method];
+        if ($path !== null) {
+            $routes[] = ['path' => $path, 'current_method' => $_SERVER['REQUEST_METHOD']];
         }
 
         // Return all routes
@@ -21,74 +21,84 @@ class Router {
     
 
     // Get and check if a route exists
-    public function getRoutes() {
+    public function getRoutes():void {
         $routes = $this->get();  // Get all routes
-        // var_dump("The routes here is");
-        // var_dump($routes);
+         var_dump("The routes here is");
+         var_dump($routes);
+
+         //Get all routes here
+
+         $all_routes=array_column(column_key:'path',array:$routes);
         
-        echo "<pre>";
-        print_r($routes);
-        echo "</pre>";
-          $all_path=array_column(array:$routes,column_key:"path");
+        // echo "<pre>";
+        // print_r($routes);
+        // echo "</pre>";
+        // var_dump("Allowed methods");
+        // var_dump($this->allowed_methods);
+           $all_path=array_column(array:$routes,column_key:"path");
           
-        //   var_dump($all_path);
+           
         //   var_dump("All methods");
           
           //Find all the on related route
 
-         $user_route_path=parse_url(url:$_SERVER['REQUEST_URI']);
+          $user_route_path=parse_url(url:$_SERVER['REQUEST_URI']);
          
-           $user_route=explode(separator:'/',string:trim($user_route_path['path']));
-        //    var_dump("User route");
-        //   var_dump($user_route[2]);
+            $user_route=explode(separator:'/',string:trim($user_route_path['path']));
+           
+            
+        
+
+         
 
           //find the method of the route
-         var_dump($user_route[2]);
+        //  var_dump($user_route[2]);
           $user_route_method=array_filter(array:$routes,callback:function($q) use ($user_route){
               return $q['path']===$user_route[2];
           });
-                
-          if(count($user_route_method)==1){
-            ;
-            ['method'=>$route_method]=$user_route_method[0];
 
-            var_dump($route_method);
+          //Current method
+          // if(count($user_route_method)){
+          //   var_dump($user_route_method[0]['current_method']);
+          // }
+          
+
+
+          //check whether the method exists or not 
+        
+
+          
+          if(count($user_route_method)===1){
+            var_dump("Route exists");
+            if(in_array(needle:$user_route_method[0]['current_method'],haystack:$this->allowed_methods)){
+              echo "Method exists";
+          }else{
+            echo "Method does not exist";
           }
+          }else{
+            die("Route does not exist");
+          }
+          // var_dump($user_route_method['current_method']);
+          // //method name is 
+          // $current_route_method=strtolower(trim($user_route_method['current_method']));
 
-         if(in_array(needle:$route_method,haystack:$this->methods)){
-             echo "method is macthed";
-         }else{
-            echo "Method not matched";
-         }
-        //   var_dump($all_path);
-        //  var_dump($user_route[2]);
-         //Find the method of the user route if its there
-        // if(in_array(needle:trim($user_route[2]),haystack:$all_path)){
-            
-        // }else{
-        //     var_dump("it is not there");
-        // }
-        // var_dump($all_path);
-        // $route = 'posts';
-        // $method = 'GET';  // Example: Check if GET request to 'posts' exists
+          // var_dump($current_route_method);
 
-        // // Search for the route with the specific method
-        // $routeFound = false;
-        // foreach ($routes as $r) {
-        //     if ($r['path'] === $route && $r['method'] === $method) {
-        //         $routeFound = true;
-        //         break;
-        //     }
-        // }
+          
+        }
 
-        // // Output result
-        // if ($routeFound) {
-        //     echo "Route found\n";
-        // } else {
-        //     echo "Route not found\n";
-        // }
+          
+                
+          // if(count($user_route_method)==1){
+          //   ;
+          //   ['method'=>$route_method]=$user_route_method[0];
+
+          //   var_dump($route_method);
+          // }
+
+        
     }
-}
+
 
 // Example usage:
 
--- routes.php
+++ routes.php
@@ -3,8 +3,10 @@
  * @var App\Router $router
  */
 
-$router->get(path:'hello',method:$_SERVER['REQUEST_METHOD']);
-$router->get(path:'hello_two',method:$_SERVER['REQUEST_METHOD']);
+$router->get(path:'post');
+$router->get(path:'post_two');
+
+
 
 
  
