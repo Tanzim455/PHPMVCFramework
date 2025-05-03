640ac6163b8802871125f33461f80e4d930904e5
Tanzim455 <tanzim.ibthesam@gmail.com>
2025-05-03 22:39:24 +0600

    Figuring out post Controller new instantiation and PostConroller::class

---
+++ App/Controller/PostController.php
@@ -0,0 +1,17 @@
+<?php 
+declare(strict_types=1);
+ namespace App\Controller;
+
+
+
+class PostController{
+    public function index(){
+        echo "Index class from Post Controller";
+    }
+    public function index_TWO(){
+        echo "Index class from Post Controller";
+    }
+}
+
+
+?>
--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controller\PostController;
 use App\Router;
 
 require '../vendor/autoload.php';
@@ -7,7 +8,48 @@ $router=new Router();
 require_once '../routes.php';
 
 
-$router->getRoutes();
+// $router->getRoutes();
+$post = PostController::class;
+
+
+$post_instantiate=new PostController();
+
+var_dump($post_instantiate);
+
+$post_controller=(object)(PostController::class);
+var_dump($post_controller->scalar);
+$new_post=new $post_controller->scalar();
+
+$new_post->index();
+die();
+
+
+
+
+
+$reflection_post = new ReflectionClass(PostController::class);
+
+echo "Reflection method is<br>";
+echo "Reflection post is<br>";
+var_dump($reflection_post);
+
+ var_dump($reflection_post->getMethods());
+
+
+$all_methods=array_column(array:$reflection_post->getMethods(),column_key:'name');
+
+var_dump($all_methods);
+
+if(($all_methods)){
+    echo "Method is there";
+}else{
+    echo "Method is not there";
+}
+
+
+// if(in_array(needle:$method_name,haystack:$reflection_post->getMethods())){
+//     echo "Method exists";
+// }
 
 
 
